73a2b330edb7eb066d04ebbcdecc0ad6cad3b43a
Susanna Fort <vande@iMac-de-Susanna.local>
2019-11-12 19:36:13 +0100

    Module that contains new Kit Version in Inventory Product, in order to sort products by KIT version. Kit Version also can be seen in Product Tree view, from Inventory module.

--- product.py
+++ product.py
@@ -7,21 +7,21 @@ _logger = logging.getLogger(__name__)
 class product_kit_version(models.Model):
 	_name = 'product.kit.version'
 	_description = 'Product Kit Version'
-	_order = "product_kit_version"
+	_order = "name"
 
-	product_kit_version = fields.Char('Product Kit Version', required=True)
-
-	@api.model
-	def create(self):
-		return super(product_kit_version, self).create(vals)
-
-	@api.multi
-	def write(self):
-		return super(product_kit_version, self).write(vals)
+	name = fields.Char('Product Kit Version', required=True)
 
 class product_template(models.Model):
-	_inherit = 'product.product'
+	_inherit = 'product.template'
+	_order = "product_kit_version_ids"
 
-	product_kit_version_id = fields.Many2many('product.kit.version', string='Product Kit Version',
-	 help="Those categories are used to group similar products for internal kit version")
+	def _get_default_product_kit_version(self):
+		cr = self.pool.cursor()
+		self.env
+		return self.pool.get('product.kit.version').search(cr, self.env.uid, [])
+
+	product_kit_version_ids = fields.Many2many('product.kit.version',
+		'product_product_kit_version_rel','product_id','product_kit_version_id', 
+		string='Product Kit Version',
+	    help="Those categories are used to group similar products for internal kit version")
 
